6700144aaaf41b7f1f4cc0997f68026628766702
PaulAmbrose <pablo_rose34@hotmail.com>
2018-09-27 12:45:17 +0100

    update

--- MusicSelector_Prototype.py
+++ MusicSelector_Prototype.py
@@ -1,23 +1,9 @@
 #"""Tool for managing music on SD cards"""
-#github address
-
-#Run loop to select file corresponding to the random number
-#Check file will fit on remaining SD card space
-#If it will transfer it, if not quit
 
 import os, os.path
 import random
 import sys
 import shutil
-
-
-#def get_size(start_path):
- #   total_size = 0
- #   for dirpath, dirnames, filenames in os.walk(start_path):
-  #      for f in filenames:
-    #        fp = os.path.join(dirpath, f)
-     #       total_size += os.path.getsize(fp)
-    #return total_size
 
 def get_number_source_files(source):
     
@@ -29,14 +15,12 @@ def main_menu():
     source = input("Please enter the source directory for your music store:  ") + "/"
     destination = input("Please enter the destination directory of your SD card:  ") + "/"
     counter = 0
-    while counter < 3:
-    #while  True:
+    while  True:
         total, used, free = shutil.disk_usage(destination)
         get_dest_capacity = total
         get_dest_files = used
         calc_dest_space = (get_dest_capacity - get_dest_files)
         random_file = random.choice(os.listdir(source))
-      #  random_file_size = get_size(random_file)
         random_file_size = os.path.getsize(source + random_file)
         print(random_file_size)
         print(calc_dest_space)
