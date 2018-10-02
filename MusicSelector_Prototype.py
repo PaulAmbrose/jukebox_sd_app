25172edf4259dcabeca9ad3b94c8143a587966ea
PaulAmbrose <pablo_rose34@hotmail.com>
2018-10-02 16:55:23 +0100

    Update

--- MusicSelector_Prototype.py
+++ MusicSelector_Prototype.py
@@ -4,6 +4,12 @@ import os, os.path
 import random
 import sys
 import shutil
+import logging
+import time
+
+datestamp = time.strftime("%c")
+logging.basicConfig(filename="Update_log_" + datestamp + "_.txt", level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_number_source_files(source):
     
@@ -14,22 +20,24 @@ def main_menu():
     print("Welcome to Paul's SD Card Jukebox Generator\n")
     source = input("Please enter the source directory for your music store:  ") + "/"
     destination = input("Please enter the destination directory of your SD card:  ") + "/"
-    percentage_fill = input("Please enter the % (in decimal e.g. 0.1 for 10%) of remaining space on your card you wish to fill:  ")
+    percentage_fill = input("Please enter the % (as int e.g.10 for 10%) of remaining space on your card you wish to fill:  ")
+    
     counter = 0
     files_added = 0
     while  True:
         total, used, free = shutil.disk_usage(destination)
-        get_dest_capacity = (total * percentage_fill)
+        get_dest_capacity = (total * (int(percentage_fill)/100))
         get_dest_files = used
-        calc_dest_space = (get_dest_capacity - sum(files_added))
+        calc_dest_space = (get_dest_capacity - files_added)
         random_file = random.choice(os.listdir(source))
         random_file_size = os.path.getsize(source + random_file)
         if random_file_size < calc_dest_space:
             source_file = source + random_file
             try:
                 shutil.copytree(source_file, destination + random_file +"/")
-                files_added = files_added + random_file_size
+                files_added = (files_added + random_file_size)
                 counter = counter +1
+                logging.debug("File_" + source_file + "_copied to_" + destination)
             except:
                 pass
         else:
