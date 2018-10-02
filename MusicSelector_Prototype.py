31062ecaf35d2ed2347e3c26deb91aaf7e93eaaa
PaulAmbrose <pablo_rose34@hotmail.com>
2018-10-02 13:42:28 +0100

    Add files via upload

--- MusicSelector_Prototype.py
+++ MusicSelector_Prototype.py
@@ -14,18 +14,21 @@ def main_menu():
     print("Welcome to Paul's SD Card Jukebox Generator\n")
     source = input("Please enter the source directory for your music store:  ") + "/"
     destination = input("Please enter the destination directory of your SD card:  ") + "/"
+    percentage_fill = input("Please enter the % (in decimal e.g. 0.1 for 10%) of remaining space on your card you wish to fill:  ")
     counter = 0
     while  True:
+        files_added = []
         total, used, free = shutil.disk_usage(destination)
-        get_dest_capacity = total
+        get_dest_capacity = (total * percentage_fill)
         get_dest_files = used
-        calc_dest_space = (get_dest_capacity - get_dest_files)
+        calc_dest_space = (get_dest_capacity - sum(files_added))
         random_file = random.choice(os.listdir(source))
         random_file_size = os.path.getsize(source + random_file)
         if random_file_size < calc_dest_space:
             source_file = source + random_file
             try:
                 shutil.copytree(source_file, destination + random_file +"/")
+                random_file.append(random_file_size)
                 counter = counter +1
             except:
                 pass
