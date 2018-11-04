8c60069538a4238162f36099f395d4267d6cb893
PaulAmbrose <pablo_rose34@hotmail.com>
2018-11-04 12:05:46 +0000

    Add files via upload

--- MusicSelector_Prototype.py
+++ MusicSelector_Prototype.py
@@ -13,15 +13,11 @@ def get_number_source_files(source):
     return total_number_files
 
 def main_menu():
-    datestamp = time.strftime("%c")
-    logging.basicConfig(filename='/home/pi/pi2_staging/Update_log_' + datestamp + '_.txt', level=logging.DEBUG, format=' %(asctime)s  -  %(levelname)s  -  %(message)s')
     print("Welcome to Paul's SD Card Jukebox Generator\n")
     source = input("Please enter the source directory for your music store:  ") + "/"
     destination = input("Please enter the destination directory of your SD card:  ") + "/"
     percentage_fill = input("Please enter the % (as int e.g.10 for 10%) of remaining space on your card you wish to fill:  ")
-    
-    counter = 0
-    files_added = 0
+ 
     while  True:
         total, used, free = shutil.disk_usage(destination)
         get_dest_capacity = (total * (int(percentage_fill)/100))
@@ -29,13 +25,9 @@ def main_menu():
         random_file_size = os.path.getsize(source + random_file)
         if random_file_size < (get_dest_capacity - used):
             source_file = source + random_file
-            print("random file size " + str(random_file_size))
-            print("calc_dest_space " + str(get_dest_capacity))
             try:
                 shutil.copytree(source_file, destination + random_file +"/")
                 files_added = (files_added + random_file_size)
-                counter = counter +1
-                logging.DEBUG("File - " + source_file +  "copied to - "  + destination) 
             except:
                 pass
         else:
